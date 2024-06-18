d062c2468e51764694d3b406248a2ef7afe5f23e
taejinnoh <shxowlstjnoh@gmail.com>
2024-06-18 13:57:32 +0900

    fix: set router options

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -13,9 +13,9 @@ func (app *Config) routes() http.Handler {
 
 	// specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "https://*"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorizaion", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300,
